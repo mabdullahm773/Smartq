e2852b06e07fd4ac610ba4e89da00a6143d4616a
Mohammad Abdullah <mabdullahm773@gmail.com>
2025-04-23 17:34:09 +0500

    File changed

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -11,7 +11,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Hello")),
+      body: Center(child: Text("Hello, Pushed to GitHub")),
     );
   }
 }
